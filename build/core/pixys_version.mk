d4f9976896103a77edd9fa274c51532db1af760f
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:34:06 -0500

    signing: support inline signing for U [2/2]
    For those who have used .android-certs previously, cp .android-certs/* vendor/aosp/signing/keys/
    
    Change-Id: If8553fc9f89cbc0923abdae46e42218df14535bf

--- build/core/pixys_version.mk
+++ build/core/pixys_version.mk
@@ -52,3 +52,13 @@ PRODUCT_GENERIC_PROPERTIES += \
   ro.pixys.fingerprint=$(PIXYS_FINGERPRINT)\
   ro.pixys.edition=$(PIXYS_EDITION)\
 
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/pixys/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/pixys/signing/keys/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard vendor/pixys/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/pixys/signing/keys/otakey.x509.pem
+endif
+endif
